83a6974639dfebde13703ce487612c0a2b96c5e9
Mikki Kaushal <mikkikaushal@mikkikashalsmbp.home>
2014-06-08 09:15:31 -0400

    added Test Class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Mikki Kaushal on 6/8/14.
+//  Copyright (c) 2014 Mikki Kaushal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Mikki Kaushal on 6/8/14.
+//  Copyright (c) 2014 Mikki Kaushal. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
