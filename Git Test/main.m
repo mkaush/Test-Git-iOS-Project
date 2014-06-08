b4886dc3a5e69818b36bcadadc0f671367fd5ce0
Mikki Kaushal <mikkikaushal@mikkikashalsmbp.home>
2014-06-07 23:12:59 -0400

    Initial Commit

---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Mikki Kaushal on 6/7/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "mkAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([mkAppDelegate class]));
+    }
+}
---
+++ Git Test/mkViewController.h
@@ -0,0 +1,13 @@
+//
+//  mkViewController.h
+//  Git Test
+//
+//  Created by Mikki Kaushal on 6/7/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mkViewController : UIViewController
+
+@end
